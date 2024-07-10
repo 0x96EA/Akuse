2b1710923707b4f1b34465048346b603a81e00c5
William Grant <0x96ea@gmail.com>
2024-07-10 22:32:23 +1000

    feat: use us as default for universal url

--- src/modules/providers/api.ts
+++ src/modules/providers/api.ts
@@ -7,6 +7,25 @@ import { getEpisodeUrl as animeunity } from './animeunity';
 import { getEpisodeUrl as gogoanime } from './gogoanime';
 import { STORAGE } from '../storage';
 
+export const getBestQualityVideo = (videos: IVideo[]): IVideo => {
+  const qualityOrder = ['1080p', '720p', '480p', '360p', 'default', 'backup'];
+
+  videos.sort((a, b) => {
+    const indexA = qualityOrder.indexOf(a.quality || 'default');
+    const indexB = qualityOrder.indexOf(b.quality || 'default');
+
+    if (indexA < indexB) return -1;
+    if (indexA > indexB) return 1;
+    return 0;
+  });
+
+  return videos[0];
+};
+
+export const getDefaultQualityVideo = (videos: IVideo[]): IVideo =>
+  videos.find(video => video.quality === 'default') ??
+  getBestQualityVideo(videos);
+
 /**
  * Gets the episode url and isM3U8 flag, with stored language and dubbed
  *
@@ -21,7 +40,9 @@ export const getUniversalEpisodeUrl = async (
   const lang = await STORAGE.getSourceFlag();
   const dubbed = await STORAGE.getDubbed();
 
-  const customTitle = animeCustomTitles[lang][listAnimeData.media?.id!];
+  const customTitle = listAnimeData.media.id
+    ? animeCustomTitles[lang][listAnimeData.media.id]
+    : null;
 
   const animeTitles = getParsedAnimeTitles(listAnimeData.media);
   if (customTitle) animeTitles.unshift(customTitle.title);
@@ -33,44 +54,24 @@ export const getUniversalEpisodeUrl = async (
   console.log(`${lang} ${dubbed} ${customTitle?.title}`);
 
   switch (lang) {
-    case 'US': {
-      const data = await gogoanime(
-        animeTitles,
-        customTitle && !dubbed ? customTitle.index : 0,
-        episode,
-        dubbed
-      );
-      return data ? getDefaultQualityVideo(data) : null;
-    }
     case 'IT': {
-      const data = await animeunity(
+      const animteUnityData = await animeunity(
         animeTitles,
         customTitle && !dubbed ? customTitle.index : 0,
         episode,
         dubbed
       );
-      return data ? getDefaultQualityVideo(data) : null; // change when animeunity api updates
+      return animteUnityData ? getDefaultQualityVideo(animteUnityData) : null; // change when animeunity api updates
+    }
+    case 'US':
+    default: {
+      const gogoAnimeData = await gogoanime(
+        animeTitles,
+        customTitle && !dubbed ? customTitle.index : 0,
+        episode,
+        dubbed
+      );
+      return gogoAnimeData ? getDefaultQualityVideo(gogoAnimeData) : null;
     }
   }
-
-  return null;
-};
-
-export const getDefaultQualityVideo = (videos: IVideo[]): IVideo =>
-  videos.find(video => video.quality === 'default') ??
-  getBestQualityVideo(videos);
-
-export const getBestQualityVideo = (videos: IVideo[]): IVideo => {
-  const qualityOrder = ['1080p', '720p', '480p', '360p', 'default', 'backup'];
-
-  videos.sort((a, b) => {
-    const indexA = qualityOrder.indexOf(a.quality || 'default');
-    const indexB = qualityOrder.indexOf(b.quality || 'default');
-
-    if (indexA < indexB) return -1;
-    if (indexA > indexB) return 1;
-    return 0;
-  });
-
-  return videos[0];
 };
